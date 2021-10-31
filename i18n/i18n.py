64ff45eb660150a8e1f056d43b63d9d56639caa4
Jeff S <jeffreesun@protonmail.com>
2021-10-31 00:54:43 +0000

    Fix i18n script to output triple quoted strings to account for quotes in translations

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -30,64 +30,79 @@ SRC_DIR = '../src'
 TRANSLATION_FILES_DIR = 'translations'
 
 def find_translation_slugs():
-	slugs = {}
-	for (dirpath, _, filenames) in walk(SRC_DIR):
-		for filename in filenames:
-			if not filename.endswith('.py'):
-				continue
-			with open(join(dirpath, filename), 'r') as f:
-				contents = f.read()
-				for match in re.findall(r'\( \'(.+?)\' \)', contents):
-					slugs[match] = True
-	return slugs
+    """Searches the src directory for all 'slugs' that should be translated
+       by looking for matches of the pattern ( 'string' )
+    """
+    slugs = {}
+    for (dirpath, _, filenames) in walk(SRC_DIR):
+        for filename in filenames:
+            if not filename.endswith('.py'):
+                continue
+            with open(join(dirpath, filename), 'r') as src_file:
+                contents = src_file.read()
+                for match in re.findall(r'\( \'(.+?)\' \)', contents):
+                    slugs[match] = True
+    return slugs
 
-def load_translations(f):
-	translations = json.load(f)
-	for slug, translation in list(translations.items()):
-		del translations[slug]
-		translations[slug.replace('\n', '\\n')] = translation.replace('\n', '\\n')
-	return translations
+def load_translations(translation_file):
+    """Loads translations from the given file and returns them as a map"""
+    translations = json.load(translation_file)
+    for slug, translation in list(translations.items()):
+        del translations[slug]
+        translations[slug.replace('\n', '\\n')] = translation.replace('\n', '\\n')
+    return translations
 
-slugs = find_translation_slugs()
+def main():
+    """Main handler"""
+    slugs = find_translation_slugs()
 
-if sys.argv[1] == 'validate':
-	translation_files = [f for f in listdir(TRANSLATION_FILES_DIR) if isfile(join(TRANSLATION_FILES_DIR, f))]
-	for translation_file in translation_files:
-		print('Validating %s...' % translation_file)
-		ok = True
-		with open(join(TRANSLATION_FILES_DIR, translation_file), 'r') as f:
-			translations = load_translations(f)
-			for slug in slugs:
-				if slug not in translations or translations[slug] == '':
-					print('Missing translation for "%s"' % slug)
-					ok = False
-			for translation_slug in translations:
-				if translation_slug not in slugs:
-					print('Unnecessary translation for "%s"' % translation_slug)
-					ok = False
-		if ok:
-			print('OK')
-elif sys.argv[1] == 'new':
-	locale = sys.argv[2]
-	translations = {}
-	for slug in slugs:
-		translations[slug.replace('\\n', '\n')] = ''
-	with open(join(TRANSLATION_FILES_DIR, '%s.json' % locale), 'w') as f:
-		f.write(json.dumps(translations, sort_keys=True, indent=4))
-elif sys.argv[1] == 'translate':
-	locale = sys.argv[2]
-	output_dir = sys.argv[3]
-	with open(join(TRANSLATION_FILES_DIR, '%s.json' % locale), 'r') as tf:
-		translations = load_translations(tf)
-		for (dirpath, _, filenames) in walk(output_dir):
-			for filename in filenames:
-				if not filename.endswith('.py'):
-					continue
-				
-				with open(join(dirpath, filename), 'r') as f:
-					contents = f.read()
-					for slug, translation in translations.items():
-						contents = contents.replace('( \'%s\' )' % slug, '\'%s\'' % translation)
-					with open(join(dirpath, filename + '.tmp'), 'w') as nf:
-						nf.write(contents)
-				shutil.move(join(dirpath, filename + '.tmp'), join(dirpath, filename))
+    if sys.argv[1] == 'validate':
+        translation_filenames = [
+            f for f in listdir(TRANSLATION_FILES_DIR)
+            if isfile(join(TRANSLATION_FILES_DIR, f))
+        ]
+        for translation_filename in translation_filenames:
+            print('Validating %s...' % translation_filename)
+            valid = True
+            with open(join(TRANSLATION_FILES_DIR, translation_filename), 'r') as translation_file:
+                translations = load_translations(translation_file)
+                for slug in slugs:
+                    if slug not in translations or translations[slug] == '':
+                        print('Missing translation for "%s"' % slug)
+                        valid = False
+                for translation_slug in translations:
+                    if translation_slug not in slugs:
+                        print('Unnecessary translation for "%s"' % translation_slug)
+                        valid = False
+            if valid:
+                print('OK')
+    elif sys.argv[1] == 'new':
+        locale = sys.argv[2]
+        translations = {}
+        for slug in slugs:
+            translations[slug.replace('\\n', '\n')] = ''
+        with open(join(TRANSLATION_FILES_DIR, '%s.json' % locale), 'w') as translation_file:
+            translation_file.write(json.dumps(translations, sort_keys=True, indent=4))
+    elif sys.argv[1] == 'translate':
+        locale = sys.argv[2]
+        output_dir = sys.argv[3]
+        with open(join(TRANSLATION_FILES_DIR, '%s.json' % locale), 'r') as translation_file:
+            translations = load_translations(translation_file)
+            for (dirpath, _, filenames) in walk(output_dir):
+                for filename in filenames:
+                    if not filename.endswith('.py'):
+                        continue
+
+                    with open(join(dirpath, filename), 'r') as src_file:
+                        contents = src_file.read()
+                        for slug, translation in translations.items():
+                            contents = contents.replace(
+                                '( \'%s\' )' % slug,
+                                '"""%s"""' % translation
+                            )
+                        with open(join(dirpath, filename + '.tmp'), 'w') as tmp_src_file:
+                            tmp_src_file.write(contents)
+                    shutil.move(join(dirpath, filename + '.tmp'), join(dirpath, filename))
+
+if __name__ == '__main__':
+    main()
