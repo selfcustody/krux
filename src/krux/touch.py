06aed41396e47f4af23ad0fe33ce46d9cc03af85
odudex <eduardo.schoenknecht@gmail.com>
2022-04-29 14:34:23 -0300

    Bug fix on touch.py
    Introduced a bug on last minute when using pylint

--- src/krux/touch.py
+++ src/krux/touch.py
@@ -94,7 +94,7 @@ class Touch:
         if time.ticks_ms() > self.last_time + TOUCH_S_PERIOD:
             self.last_time = time.ticks_ms()
             data = self.touch_driver.current_point()
-            if isinstance(data) is tuple:
+            if isinstance(data, tuple):
                 self.extract_index(data)
             elif data is None:  # gets realease than return to ilde.
                 if self.state == Touch.release:
