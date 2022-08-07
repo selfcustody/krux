9a08236fa63149337d569f36026fcd7fffea5283
Jeff S <jeffreesun@protonmail.com>
2022-08-07 02:11:34 +0000

    Change copyright notice

--- simulator/kruxsim/mocks/lcd.py
+++ simulator/kruxsim/mocks/lcd.py
@@ -1,3 +1,24 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2021-2022 Krux contributors
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import sys
 import os
 from unittest import mock
@@ -98,6 +119,7 @@ def draw_string(x, y, s, color, bgcolor=COLOR_BLACK):
 
 def draw_qr_code(offset_y, code_str, max_width, dark_color, light_color):
     offset_y += 5
+
     def run():
         starting_size = 0
         while code_str[starting_size] != "\n":
