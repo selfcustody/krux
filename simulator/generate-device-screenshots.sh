6f46b6140468ca713487a61b3dd2f6a6ce52d616
Jeff S <jeffreesun@protonmail.com>
2022-08-17 12:45:19 +0000

    Update sequences to account for new settings menu

--- simulator/generate-device-screenshots.sh
+++ simulator/generate-device-screenshots.sh
@@ -30,7 +30,7 @@ rm -rf sd && mkdir -p sd && rm -f sd/settings.json
 echo "{\"settings\": {\"i18n\": {\"locale\": \"$locale\"}}}" > sd/settings.json
 
 poetry run python simulator.py --sequence sequences/about.txt --with-sd True --device $device
-poetry run python simulator.py --sequence sequences/debug-options.txt --with-sd True --device $device
+poetry run python simulator.py --sequence sequences/logging-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/extended-public-key-wpkh.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/extended-public-key-wsh.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/home-options.txt --with-sd True --device $device
@@ -39,17 +39,18 @@ poetry run python simulator.py --sequence sequences/load-mnemonic-via-bits.txt -
 poetry run python simulator.py --sequence sequences/load-mnemonic-via-numbers.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/load-mnemonic-via-qr.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/load-mnemonic-via-text.txt --with-sd True --device $device
-poetry run python simulator.py --sequence sequences/locale-options.txt --with-sd True --device $device
+poetry run python simulator.py --sequence sequences/language-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/login-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/logo.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/mnemonic-12-word.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/mnemonic-24-word.txt --with-sd True --device $device
-poetry run python simulator.py --sequence sequences/network-options.txt --with-sd True --device $device
+poetry run python simulator.py --sequence sequences/bitcoin-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/new-mnemonic-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/new-mnemonic-via-d6.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/new-mnemonic-via-d20.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/print-qr.txt --with-printer True --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/printer-options.txt --with-sd True --device $device
+poetry run python simulator.py --sequence sequences/thermal-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/scan-address.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/settings-options.txt --with-sd True --device $device
 poetry run python simulator.py --sequence sequences/shutdown.txt --with-sd True --device $device
