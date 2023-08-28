37b620bacfccece0bfc3dca2233c3b76461ef912
tadeubas <tadeubas@gmail.com>
2023-08-28 20:42:48 -0300

    Fixed BW wallet case when user cancel load

--- src/krux/pages/home.py
+++ src/krux/pages/home.py
@@ -230,16 +230,17 @@ class Home(Page):
                 )
                 self.ctx.display.flash_text(t("Wallet output descriptor loaded!"))
 
-            # BlueWallet single sig descriptor without fingerprint
-            if (
-                self.ctx.wallet.descriptor.key
-                and not self.ctx.wallet.descriptor.key.origin
-            ):
-                self.ctx.display.clear()
-                self.ctx.display.draw_centered_text(
-                    t("Warning:\nIncomplete output descriptor"), theme.error_color
-                )
-                self.ctx.input.wait_for_button()
+                # BlueWallet single sig descriptor without fingerprint
+                if (
+                    self.ctx.wallet.descriptor.key
+                    and not self.ctx.wallet.descriptor.key.origin
+                ):
+                    self.ctx.display.clear()
+                    self.ctx.display.draw_centered_text(
+                        t("Warning:\nIncomplete output descriptor"), theme.error_color
+                    )
+                    self.ctx.input.wait_for_button()
+
         except Exception as e:
             self.ctx.log.exception("Exception occurred loading wallet")
             self.ctx.display.clear()
