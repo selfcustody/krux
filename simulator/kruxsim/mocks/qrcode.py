32e87174c871af0ec7eb41bb19a36dddf50708a1
Odudex <eduardo.schoenknecht@gmail.com>
2023-11-14 12:48:30 -0300

    simulator: compatibility to binary image QR code

--- simulator/kruxsim/mocks/qrcode.py
+++ simulator/kruxsim/mocks/qrcode.py
@@ -53,8 +53,29 @@ def encode_to_string(data):
 
     return new_code_str
 
+def encode(data):
+    # Uses string encoded qr as it already cleaned up the frames
+    # PyQRcode also doesn't offer any binary output
+
+    frame_less_qr = encode_to_string(data)
+    size = 0
+    while frame_less_qr[size] != "\n":
+        size += 1
+    binary_qr = bytearray(b"\x00" * ((size * size + 7) // 8))                                    
+    for y in range(size):
+        for x in range(size):
+            bit_index = y * size + x
+            bit_string_index = y * (size + 1) + x
+            if frame_less_qr[bit_string_index] == "1":
+                binary_qr[bit_index>>3] |= 1 << (bit_index % 8)
+    return binary_qr
+
+
+    
+
 
 if "qrcode" not in sys.modules:
     sys.modules["qrcode"] = mock.MagicMock(
         encode_to_string=encode_to_string,
+        encode = encode,
     )
