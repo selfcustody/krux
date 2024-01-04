4ff4eb2c41c4a0e5467c3756737384e11f87f76b
qlrd <qlrd 106913782+qlrd@users.noreply.github.com>
2024-01-04 07:19:00 -0300

    added some black formatting and lint to test/test_context.py

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,7 +1,22 @@
-from .shared_mocks import get_mock_open
+# TODO: cleanup .shared_mocks.get_mock_open?
+# tests go well without calling it
+# pylint calls warning `unused-import`
+# W0611: Unused get_mock_open imported from shared_mocks (unused-import)
+# from .shared_mocks import get_mock_open
 
 
 def mock_modules(mocker):
+    """
+    Utility method to patch :module:`krux.context` classes:
+
+    - :class:`krux.context.logger`
+    - :class:`krux.context.Display`
+    - :class:`krux.context.Input`
+    - :class:`krux.context.Camera`
+    - :class:`krux.context.Light`
+
+    :param mocker
+    """
     mocker.patch("krux.context.logger", new=mocker.MagicMock())
     mocker.patch("krux.context.Display", new=mocker.MagicMock())
     mocker.patch("krux.context.Input", new=mocker.MagicMock())
@@ -9,45 +24,76 @@ def mock_modules(mocker):
     mocker.patch("krux.context.Light", new=mocker.MagicMock())
 
 
+# pylint: disable=unused-argument
 def test_init(mocker, m5stickv):
+    """
+    Test the initialization of :class:`krux.context.Context`
+    """
     mock_modules(mocker)
     from krux.context import Context
 
-    c = Context()
+    # Avoid pylint `invalid-name` warning
+    # 0103: Variable name "c" doesn't conform to snake_case naming style
+    context = Context()
 
-    assert isinstance(c, Context)
+    assert isinstance(context, Context)
 
 
+# pylint: disable=unused-argument
 def test_clear(mocker, m5stickv):
+    """
+    Test the cleanup of :class:`krux.context.Context`
+    """
     mock_modules(mocker)
     from krux.context import Context
 
-    c = Context()
+    # Avoid pylint `invalid-name` warning
+    # 0103: Variable name "c" doesn't conform to snake_case naming style
+    context = Context()
 
-    c.clear()
+    context.clear()
 
-    assert c.wallet is None
+    assert context.wallet is None
 
 
+# pylint: disable=unused-argument
 def test_log(mocker, m5stickv):
+    """
+    Test the initialization of :class:`krux.context.logger`
+    inside the :class:`krux.context.Context`
+    """
     mock_modules(mocker)
     import krux
     from krux.context import Context
 
-    c = Context()
+    # Avoid pylint `invalid-name` warning
+    # 0103: Variable name "c" doesn't conform to snake_case naming style
+    context = Context()
 
-    assert c.log == krux.context.logger
+    assert context.log == krux.context.logger
 
 
+# pylint: disable=unused-argument
 def test_clear_clears_printer(mocker, m5stickv):
+    """
+    Test the initialization and cleanup of :class:`krux.context.Printer`
+    inside the :class:`krux.context.Context`
+    """
     mock_modules(mocker)
     from krux.context import Context
-    from krux.printers import Printer
 
-    c = Context()
-    c.printer = mocker.MagicMock(clear=mocker.MagicMock())
+    # TODO: cleanup krux.printer.Printer?
+    # tests go well without calling it
+    # pylint calls warning `unused-import`
+    # W0611: Unused Printer imported from krux.printers (unused-import)
+    # from krux.printers import Printer
 
-    c.clear()
+    # Avoid pylint `invalid-name` warning
+    # 0103: Variable name "c" doesn't conform to snake_case naming style
+    context = Context()
+    context.printer = mocker.MagicMock(clear=mocker.MagicMock())
 
-    assert c.wallet is None
-    c.printer.clear.assert_called()
+    context.clear()
+
+    assert context.wallet is None
+    context.printer.clear.assert_called()
