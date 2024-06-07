ca134864e3db28f1a9ff9e95fbb7327f2ff24e96
odudex <eduardo.schoenknecht@gmail.com>
2024-06-07 13:31:13 -0300

    sans key loading - tests fixed

--- tests/pages/home_pages/test_wallet_descriptor.py
+++ tests/pages/home_pages/test_wallet_descriptor.py
@@ -17,31 +17,31 @@ def test_wallet(mocker, m5stickv, tdata):
             None,
             [BUTTON_PAGE],
         ),
-        # 1 Load, good data, accept
+        # 1 Load, from camera, good data, accept
         (
             False,
             tdata.SINGLESIG_12_WORD_KEY,
             tdata.SPECTER_SINGLESIG_WALLET_DATA,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
-        # 2 Load, good data, decline
+        # 2 Load, from camera, good data, decline
         (
             False,
             tdata.SINGLESIG_12_WORD_KEY,
             tdata.SPECTER_SINGLESIG_WALLET_DATA,
             None,
-            [BUTTON_ENTER, BUTTON_PAGE],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_PAGE],
         ),
-        # 3 Load, bad capture
-        (False, tdata.SINGLESIG_12_WORD_KEY, None, None, [BUTTON_ENTER]),
-        # 4 Load, bad wallet data
+        # 3 Load, from camera, bad capture
+        (False, tdata.SINGLESIG_12_WORD_KEY, None, None, [BUTTON_ENTER, BUTTON_ENTER]),
+        # 4 Load, from camera, bad wallet data
         (
             False,
             tdata.SINGLESIG_12_WORD_KEY,
             "{}",
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 5 No print prompt
         (
@@ -81,7 +81,7 @@ def test_wallet(mocker, m5stickv, tdata):
             tdata.LEGACY1_KEY,
             tdata.VAGUE_LEGACY1_XPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 10 vague BlueWallet-ish p2pkh w/o key loaded, requires allow_assumption
         (
@@ -89,7 +89,7 @@ def test_wallet(mocker, m5stickv, tdata):
             None,
             tdata.VAGUE_LEGACY1_XPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 11 vague BlueWallet-ish p2sh-p2wpkh ypub
         (
@@ -97,7 +97,7 @@ def test_wallet(mocker, m5stickv, tdata):
             tdata.NESTEDSW1_KEY,
             tdata.VAGUE_NESTEDSW1_YPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 12 vague BlueWallet-ish p2sh-p2wpkh ypub w/o key loaded
         (
@@ -105,7 +105,7 @@ def test_wallet(mocker, m5stickv, tdata):
             None,
             tdata.VAGUE_NESTEDSW1_YPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 13 vague BlueWallet-ish p2wpkh zpub
         (
@@ -113,7 +113,7 @@ def test_wallet(mocker, m5stickv, tdata):
             tdata.NATIVESW1_KEY,
             tdata.VAGUE_NATIVESW1_ZPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
         # 14 vague BlueWallet-ish p2wpkh zpub w/o key loaded
         (
@@ -121,7 +121,7 @@ def test_wallet(mocker, m5stickv, tdata):
             None,
             tdata.VAGUE_NATIVESW1_ZPUB,
             None,
-            [BUTTON_ENTER, BUTTON_ENTER],
+            [BUTTON_ENTER, BUTTON_ENTER, BUTTON_ENTER],
         ),
     ]
 
