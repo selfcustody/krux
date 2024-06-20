3bef01345b6658bca4d497c4a34731c93906eb43
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-06-20 16:52:37 -0400

    context tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -37,3 +37,31 @@ def test_clear_clears_printer(mocker, m5stickv):
 
     assert c.wallet is None
     c.printer.clear.assert_called()
+
+
+def test_clear_clears_wallet(mocker, m5stickv):
+    mock_modules(mocker)
+    from krux.context import Context
+    from krux.wallet import Wallet
+
+    c = Context()
+    c.wallet = Wallet(None)
+
+    c.clear()
+
+    assert c.wallet is None
+
+
+def test_is_logged_in(mocker, m5stickv):
+    mock_modules(mocker)
+    from krux.context import Context
+    from krux.wallet import Wallet
+    from krux.key import Key
+
+    c = Context()
+
+    c.wallet = Wallet(None)
+    assert c.is_logged_in() == False
+
+    c.wallet = Wallet(Key(mnemonic="abandon "*11 + "about", multisig=False))
+    assert c.is_logged_in() == True
