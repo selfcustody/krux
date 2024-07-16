20de2f0b3bf68c3a11b39558163fbb70c68161e8
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-07-16 09:06:27 -0400

    another "lambda: None" becomes "lambda: MENU_EXIT"

--- src/krux/pages/home_pages/sign_message_ui.py
+++ src/krux/pages/home_pages/sign_message_ui.py
@@ -24,7 +24,7 @@ import gc
 from embit import bip32, compact
 import hashlib
 import binascii
-from .. import MENU_CONTINUE, LOAD_FROM_CAMERA, LOAD_FROM_SD, Menu
+from .. import MENU_CONTINUE, MENU_EXIT, LOAD_FROM_CAMERA, LOAD_FROM_SD, Menu
 from ...themes import theme
 from ...display import (
     DEFAULT_PADDING,
@@ -199,7 +199,7 @@ class SignMessage(Utils):
                     t("Sign to SD card"),
                     None if not self.has_sd_card() else lambda: None,
                 ),
-                ("< " + t("Back"), lambda: None),
+                ("< " + t("Back"), lambda: MENU_EXIT),
             ],
         )
         index, _ = sign_menu.run_loop()
