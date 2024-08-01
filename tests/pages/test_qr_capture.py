00600ac2e8624564c04f3cb13f404dcecd6fd337
odudex <eduardo.schoenknecht@gmail.com>
2024-08-01 13:03:14 -0300

    update tests to QR capture refactor - part IV

--- tests/pages/test_qr_capture.py
+++ tests/pages/test_qr_capture.py
@@ -5,6 +5,9 @@ from krux.qr import FORMAT_PMOFN
 from ..shared_mocks import (
     snapshot_generator,
     SNAP_ANIMATED_QR,
+    DONT_FIND_ANYTHING,
+    TimeMocker,
+    mock_context,
 )
 
 
@@ -39,41 +42,33 @@ def test_capture_qr_code(mocker, m5stickv, tdata):
     ctx.display.draw_centered_text.assert_has_calls([mocker.call("Loading Camera..")])
 
 
-# def test_camera_antiglare_7740(mocker, m5stickv, mock_page_cls):
-#     from krux.camera import Camera, OV7740_ID, OV2640_ID, GC2145_ID
+def test_camera_antiglare(mocker, m5stickv):
+    from krux.pages.qr_capture import QRCodeCapture
+    from krux.camera import Camera, OV7740_ID, OV2640_ID, GC2145_ID
 
-#     time_mocker = TimeMocker(1001)
+    time_mocker = TimeMocker(1001)
 
-#     mocker.patch(
-#         "krux.camera.sensor.snapshot",
-#         new=snapshot_generator(outcome=DONT_FIND_ANYTHING),
-#     )
-#     cameras = [OV7740_ID, OV2640_ID, GC2145_ID]
-#     for cam_id in cameras:
-#         mocker.patch("krux.camera.sensor.get_id", lambda: cam_id)
-#         mocker.patch("krux.camera.QRPartParser", new=MockQRPartParser)
+    ctx = mock_context(mocker)
+    mocker.patch.object(
+        ctx.camera,
+        "snapshot",
+        new=snapshot_generator(outcome=DONT_FIND_ANYTHING),
+    )
+    cameras = [OV7740_ID, OV2640_ID, GC2145_ID]
+    for cam_id in cameras:
+        mocker.patch.object(ctx.camera.sensor, "get_id", lambda: cam_id)
+        PAGE_SEQ = [False, True, False, True, False]
+        PAGE_PREV_SEQ = [False, False, False, False, True]
+        mocker.patch("time.ticks_ms", time_mocker.tick)
+        ctx.input.page_event = mocker.MagicMock(side_effect=PAGE_SEQ)
+        ctx.input.page_prev_event = mocker.MagicMock(side_effect=PAGE_PREV_SEQ)
+        mocker.spy(ctx.camera, "toggle_antiglare")
+        qr_capturer = QRCodeCapture(ctx)
+        qr_code, qr_format = qr_capturer.qr_capture_loop()
+        assert qr_code == None
+        ctx.camera.toggle_antiglare.assert_has_calls([mocker.call()] * 2)
+        ctx.light.turn_on.call_count == 0
 
-#         ctx = mock_context(mocker)
-#         ENTER_SEQ = [False, True, False, True, False]
-#         PAGE_PREV_SEQ = [False, False, False, True]
-#         mocker.patch("time.ticks_ms", time_mocker.tick)
-#         ctx.input.enter_event = mocker.MagicMock(side_effect=ENTER_SEQ)
-#         ctx.input.page_event = mocker.MagicMock(side_effect=ENTER_SEQ)
-#         ctx.input.page_prev_event = mocker.MagicMock(side_effect=PAGE_PREV_SEQ)
-#         ctx.camera = Camera()
-#         ctx.camera.cam_id = OV7740_ID
-#         mocker.spy(ctx.camera, "disable_antiglare")
-#         mocker.spy(ctx.camera, "enable_antiglare")
-#         mocker.spy(ctx.light, "turn_on")
-#         mocker.spy(ctx.light, "turn_off")
-#         page = mock_page_cls(ctx)
-
-#         qr_code, _ = page.capture_qr_code()
-#         assert qr_code == None
-#         ctx.camera.disable_antiglare.assert_called_once()
-#         ctx.camera.enable_antiglare.assert_called_once()
-#         ctx.light.turn_on.call_count == 2
-#         ctx.light.turn_off.call_count == 2
 
 # def test_capture_qr_code_loop(mocker, all_devices):
 #     mocker.patch(
