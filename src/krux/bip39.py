cbb69fd96df7b4d045b1c93ef6fec8ef7deaa8dd
tadeubas <tadeubas@gmail.com>
2024-08-12 20:01:39 -0300

    pylint for bip39

--- src/krux/bip39.py
+++ src/krux/bip39.py
@@ -1,15 +1,14 @@
 # Mnemonic convertion to seed and to/from bytes
 import hashlib
-from embit.misc import const
-
 from embit.wordlists.bip39 import WORDLIST
-
-PBKDF2_ROUNDS = const(2048)
 
 WORDINDEX = {word: i for i, word in enumerate(WORDLIST)}
 
 
-def mnemonic_to_bytes(mnemonic: str, ignore_checksum: bool = False, wordlist=WORDLIST):
+def mnemonic_to_bytes(
+    mnemonic: str, ignore_checksum: bool = False, wordlist=tuple(WORDLIST)
+):
+    """Verifies the mnemonic checksum and returns it in bytes"""
     words = mnemonic.strip().split()
     if len(words) % 3 != 0 or len(words) < 12:
         raise ValueError("Invalid recovery phrase")
@@ -36,10 +35,10 @@ def mnemonic_to_bytes(mnemonic: str, ignore_checksum: bool = False, wordlist=WOR
     return data
 
 
-def mnemonic_is_valid(mnemonic: str, wordlist=WORDLIST):
+def mnemonic_is_valid(mnemonic: str, wordlist=tuple(WORDLIST)):
     """Checks if mnemonic is valid (checksum and words)"""
     try:
         mnemonic_to_bytes(mnemonic, wordlist=wordlist)
         return True
-    except Exception as e:
+    except:
         return False
