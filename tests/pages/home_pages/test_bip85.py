a39ccc49a719f42ec4c8f45cc4d59fdc097df696
odudex <eduardo.schoenknecht@gmail.com>
2024-10-16 10:56:33 -0300

    BIP85 passwords: add tests

--- tests/pages/home_pages/test_bip85.py
+++ tests/pages/home_pages/test_bip85.py
@@ -1,7 +1,7 @@
 from .test_home import tdata, create_ctx
 
 
-def test_bip85_wallet_creation(mocker, amigo, tdata):
+def test_bip85_bip39_mnemonic_derivation(mocker, amigo, tdata):
     from krux.krux_settings import Settings
     from krux.settings import THIN_SPACE
     from krux.pages.home_pages.bip85 import Bip85
@@ -175,3 +175,170 @@ def test_bip85_wallet_creation(mocker, amigo, tdata):
     mocker.spy(bip85_ui, "display_mnemonic")
     bip85_ui.export()
     ctx.display.draw_centered_text.assert_called_with("⊚" + THIN_SPACE + "%s" % case[3])
+
+
+def test_bip85_base64_password_derivation(mocker, amigo, tdata):
+    from krux.pages.home_pages.bip85 import Bip85
+    from krux.wallet import Wallet
+    from krux.input import BUTTON_ENTER, BUTTON_PAGE, BUTTON_PAGE_PREV
+
+    cases = [
+        # case
+        # (
+        # wallet
+        # Button presses
+        # Password info
+        # QR data
+        # Save to SD card (True/False)
+        # )
+        # 0 - 12w input, check the password and leave
+        (
+            tdata.SINGLESIG_12_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Keep length = 21
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                BUTTON_PAGE_PREV,  # Move to "< Back"
+                BUTTON_ENTER,  # Leave
+            ],
+            "O/IkMwPt/B6R5QC/hOILV\n\nIndex: 1\nLength: 21",
+        ),
+        # 1 - 24w input, check the password and leave
+        (
+            tdata.SINGLESIG_24_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Keep length = 21
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                BUTTON_PAGE_PREV,  # Move to "< Back"
+                BUTTON_ENTER,  # Leave
+            ],
+            "RmM+TPrN8L/7gpvfeqczo\n\nIndex: 1\nLength: 21",
+        ),
+        # 2 - invalid password length, retry
+        (
+            tdata.SINGLESIG_12_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                BUTTON_ENTER,  # Append 1 to (211)
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Wait for the error message
+                BUTTON_PAGE_PREV,  # Move to "Go" again
+                BUTTON_ENTER,  # Go with 21
+                BUTTON_PAGE_PREV,  # Move to "< Back"
+                BUTTON_ENTER,  # Leave
+            ],
+            "O/IkMwPt/B6R5QC/hOILV\n\nIndex: 1\nLength: 21",
+        ),
+        # 3 - ESC at password length input
+        (
+            tdata.SINGLESIG_12_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                *([BUTTON_PAGE_PREV] * 2),  # Move to "Esc"
+                BUTTON_ENTER,  # Press "Esc"
+                BUTTON_ENTER,  # Confirm
+            ],
+            None,
+        ),
+        # 4 - Print QR code
+        (
+            tdata.SINGLESIG_12_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Keep length = 21
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Print QR code
+                BUTTON_ENTER,  # Print QR code
+                BUTTON_ENTER,  # Back main menu
+                BUTTON_PAGE_PREV,  # Move to "< Back"
+                BUTTON_ENTER,  # Leave
+            ],
+            "O/IkMwPt/B6R5QC/hOILV\n\nIndex: 1\nLength: 21",
+            "O/IkMwPt/B6R5QC/hOILV",
+        ),
+        # 5 - Test save to SD card
+        (
+            tdata.SINGLESIG_12_WORD_KEY,
+            [
+                BUTTON_PAGE,  # Move to "Base64 Password"
+                BUTTON_ENTER,  # Confirm
+                BUTTON_ENTER,  # Index 1
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Keep length = 21
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Go
+                # Save to SD card
+                BUTTON_PAGE,  # Move to "Save to SD card"
+                BUTTON_ENTER,  # Press to save
+                BUTTON_ENTER,  # Confirm
+                # Confirm file name
+                BUTTON_PAGE_PREV,  # Move to "Go"
+                BUTTON_ENTER,  # Save
+                BUTTON_PAGE_PREV,  # Move to "< Back"
+                BUTTON_ENTER,  # Leave
+            ],
+            "O/IkMwPt/B6R5QC/hOILV\n\nIndex: 1\nLength: 21",
+            None,
+            True,
+        ),
+    ]
+    mock_save_file = mocker.patch(
+        "krux.pages.file_operations.SaveFile.save_file",
+    )
+    case_num = 0
+    for case in cases:
+        print(case_num)
+        wallet = Wallet(case[0])
+        ctx = create_ctx(mocker, case[1], wallet)
+        mocker.spy(ctx.display, "draw_hcentered_text")
+        bip85_ui = Bip85(ctx)
+        mocker.patch.object(bip85_ui, "has_sd_card", new=lambda: True)
+        mocker.spy(bip85_ui, "_base64_password_qr")
+        bip85_ui.export()
+
+        assert ctx.input.wait_for_button.call_count == len(case[1])
+        if case[2]:
+            ctx.display.draw_hcentered_text.assert_has_calls(
+                [mocker.call(case[2], info_box=True)]
+            )
+        if len(case) > 3 and case[3]:
+            bip85_ui._base64_password_qr.assert_has_calls(
+                [mocker.call(case[3], "Base64 Password")]
+            )
+
+        if len(case) > 4 and case[4]:
+            mock_save_file.assert_called_once_with(
+                case[2],
+                "BIP85_password",
+                "BIP85_password",
+                "BIP85 Password:",
+                ".txt",
+                save_as_binary=False,
+            )
+        case_num += 1
