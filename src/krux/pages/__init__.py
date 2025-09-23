5d3b808b4df54f9211245a98d61c0a4d347a99b4
odudex <odudex@proton.me>
2025-09-23 10:29:16 -0300

    better centralize menu when there's no status bar

--- src/krux/pages/__init__.py
+++ src/krux/pages/__init__.py
@@ -635,10 +635,11 @@ class Menu:
             self.ctx.wallet is None and not kboard.has_battery
         )
         if offset is None:
-            # Default offset for status bar
-            self.menu_offset = STATUS_BAR_HEIGHT
+            self.menu_offset = (
+                STATUS_BAR_HEIGHT if not self.disable_statusbar else DEFAULT_PADDING
+            )
         else:
-            # Always diasble status bar if menu has non standard offset
+            # Always disable status bar if menu has non standard offset
             self.disable_statusbar = True
             self.menu_offset = offset if offset >= 0 else DEFAULT_PADDING
         max_viewable = min(
@@ -920,6 +921,9 @@ class Menu:
         y_keypad_map = [
             int(n * height_multiplier) + self.menu_offset for n in y_keypad_map
         ]
+        # Expand first region to fill the screen if there's nothing above it
+        if y_keypad_map[0] < STATUS_BAR_HEIGHT:
+            y_keypad_map[0] = 0
         # Expand last region to fill the screen
         y_keypad_map[-1] = self.ctx.display.height()
         self.ctx.input.touch.y_regions = y_keypad_map
