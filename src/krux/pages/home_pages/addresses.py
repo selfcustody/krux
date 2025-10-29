cb9839cf85d77f2fd89f2bf09d30f4d2d1d9fe48
Jean Do <117163651+jdlcdl@users.noreply.github.com>
2025-10-29 19:47:59 -0400

    BugFix: Passphrase (via encrypted mnemonic entropy) fails gracefully (#756)
    * resolve bug found by @tadeubas around encrypted passphrases
      In Cpython a UnicodeDecodeError was being caught/ignored by the ValueError,
      while in MaixPy, a TypeError was being raised, not caught, bubbling up:
      ...whenever an encrypted passphrase returned bytes that could not be decoded to a string.
    Symptoms:
      on simulator: appeared as if NOT KEF or user declines to decrypt,
      on k210: TypeError("Can't convert 'int' object to str implicitely",)
    
    * Passphrases: Ensure they are ASCII strings
    
    * better error handling for decrypted wallet descriptors
    
    * Similar pattern fix for encrypted addresses that don't decode
      decoding of plaintext data is done separately in it's on
      try except block to catch decoding errors and fail early.
    
    ---------
    
    Co-authored-by: odudex <odudex@proton.me>

--- src/krux/pages/home_pages/addresses.py
+++ src/krux/pages/home_pages/addresses.py
@@ -297,7 +297,14 @@ class Addresses(Page):
             return MENU_CONTINUE
 
         try:
-            data = decrypt_kef(self.ctx, data).decode()
+            data = decrypt_kef(self.ctx, data)
+
+            # Cpython raises UnicodeDecodeError, MaixPy raises TypeError
+            try:
+                data = data.decode()
+            except:
+                self.flash_error(t("Failed to load"))
+                return MENU_CONTINUE
         except KeyError:
             self.flash_error(t("Failed to decrypt"))
             return MENU_CONTINUE
