selfcustody/krux

commit f15e198e180a6da277297b429edfc64bcc67fa2d

fix: pMofN QRs now deal with bytes data

src/krux/qr.py
@@ -372,6 +372,7 @@ def find_min_num_parts(data, max_width, qr_format):
 
 def parse_pmofn_qr_part(data):
     """Parses the QR as a P M-of-N part, extracting the part's content, index, and total"""
+    data = data.decode() if isinstance(data, bytes) else data
     of_index = data.index("of")
     space_index = data.index(" ")
     part_index = int(data[1:of_index])
@@ -382,6 +383,7 @@ def parse_pmofn_qr_part(data):
 def detect_format(data):
     """Detects the QR format of the given data"""
     qr_format = FORMAT_NONE
+    data = data.decode() if isinstance(data, bytes) else data
     try:
         if data.startswith("p"):
             header = data.split(" ")[0]

commit 38325dc79fe94ca7b01638e9c0777e2881ab6644

fix: Compact SeedQR broken by commit f15e198 (#794)
still adjusting the code for true binary QRs

src/krux/qr.py
@@ -383,8 +383,8 @@ def parse_pmofn_qr_part(data):
 def detect_format(data):
     """Detects the QR format of the given data"""
     qr_format = FORMAT_NONE
-    data = data.decode() if isinstance(data, bytes) else data
     try:
+        data = data.decode() if isinstance(data, bytes) else data
         if data.startswith("p"):
             header = data.split(" ")[0]
             if "of" in header and header[1:].split("of")[0].isdigit():