4894762fd8aacc415ce8f59d6a6d84d724d30c59
Naman015 <namanguptaoct15@gmail.com>
2026-04-17 22:53:01 +1000

    style: fix black formatting for flash_success method

--- src/krux/pages/__init__.py
+++ src/krux/pages/__init__.py
@@ -138,11 +138,11 @@ class Page:
         """Flashes text centered on the display for duration ms"""
         self.flash_text(text, theme.error_color)
 
-    def flash_success(
-        self, text, duration=FLASH_MSG_TIME, highlight_prefix=""
-    ):
+    def flash_success(self, text, duration=FLASH_MSG_TIME, highlight_prefix=""):
         """Flashes success text centered on the display for duration ms"""
-        self.flash_text(text, theme.go_color, duration, highlight_prefix=highlight_prefix)
+        self.flash_text(
+            text, theme.go_color, duration, highlight_prefix=highlight_prefix
+        )
 
     # pylint: disable=too-many-arguments
     def capture_from_keypad(
